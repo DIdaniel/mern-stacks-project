9bc71cc4a4dfcdee403f6c83e4541cd6042577ef
KIM DONG IL <dongil.kim@powertask.biz>
2022-02-13 16:32:23 +0900

    Landing Page styled component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import Landing from "./pages/Landing.jsx";
+import styled from "styled-components/macro";
+
+const Button = styled.button`
+  background: red;
+  color: #fff;
+  font-size: 1rem;
+`;
+
+const ButtonSecond = styled.button`
+  background: blue;
+  color: #fff;
+  font-size: 1rem;
+`;
 
 function App() {
-  return <div className="App">jobfy</div>;
+  return (
+    <div>
+      <Landing />
+    </div>
+  );
 }
 
 export default App;
